36ebcc20fcbfcb32835829fb294a6c5a7a2848ca
KKrntzk <sophie.kornetzki@gmail.com>
2025-12-15 17:05:39 +0100

    endboss added

--- models/character.class.js
+++ models/character.class.js
@@ -26,12 +26,10 @@ class Character extends MovableObject {
       }
       this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
+
     setInterval(() => {
       if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-        let i = this.currentImage % this.imagesWalking.length;
-        let path = this.imagesWalking[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
+        this.playAnimation(this.imagesWalking);
       }
     }, 50);
   }
--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,1 +1,19 @@
-class Endboss extends MovableObject {}
+class Endboss extends MovableObject {
+  height = 400;
+  width = 250;
+  y = 60;
+
+  imagesWalking = ImageHub.endboss.alert;
+  constructor() {
+    super().loadImg("img_pollo_locco/img/4_enemie_boss_chicken/1_walk/G1.png");
+    this.loadImages(this.imagesWalking);
+    this.x = 2000;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.imagesWalking);
+    }, 170);
+  }
+}
