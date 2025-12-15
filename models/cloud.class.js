886ecef4efc5be739a8eadae029e61536af5ef8f
KKrntzk <sophie.kornetzki@gmail.com>
2025-12-15 12:13:55 +0100

    clouds animated

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,10 +1,17 @@
 class Cloud extends MovableObject {
   width = 700;
-  height = 200;
+  height = 300;
+  y = 20;
 
   constructor() {
     super().loadImg("img_pollo_locco/img/5_background/layers/4_clouds/1.png");
     this.x = Math.random() * 500;
-    this.y = 20;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.x -= 0.15;
+    }, 1000 / 60);
   }
 }
