2c36ad26cdfdc92bdeba3313505e488a0c7bd966
KKrntzk <sophie.kornetzki@gmail.com>
2025-12-15 11:59:23 +0100

    scaling changed

--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,6 @@
 class Character extends MovableObject {
+  height = 300;
+  y = 135;
   constructor() {
     super().loadImg("img_pollo_locco/img/2_character_pepe/2_walk/W-21.png");
   }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,7 @@
 class Chicken extends MovableObject {
-  y = 335;
+  y = 370;
+  height = 60;
+  width = 60;
   constructor() {
     super().loadImg(
       "img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png"
