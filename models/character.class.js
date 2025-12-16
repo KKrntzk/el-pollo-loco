98fa2fe9983a9668a79f0de4586a38a0b33cb392
KKrntzk <sophie.kornetzki@gmail.com>
2025-12-16 12:02:10 +0100

    code cleaned, outsourced animate sections

--- models/character.class.js
+++ models/character.class.js
@@ -19,17 +19,17 @@ class Character extends MovableObject {
   animate() {
     setInterval(() => {
       if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-        this.x += this.speed;
-        this.otherDirection = false;
+        this.moveRight();
       }
 
       if (this.world.keyboard.LEFT && this.x > 0) {
-        this.x -= this.speed;
-        this.otherDirection = true;
+        this.moveLeft();
       }
-      if (this.world.keyboard.UP && !this.isAboveGround()) {
-        this.speedY = 30;
+
+      if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+        this.jump();
       }
+
       this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
 
@@ -43,6 +43,4 @@ class Character extends MovableObject {
       }
     }, 50);
   }
-
-  jump() {}
 }
