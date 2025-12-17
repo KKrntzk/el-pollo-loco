f50e0ea66eb9ce28910abe03679172cd3f7153a3
KKrntzk <sophie.kornetzki@gmail.com>
2025-12-17 08:41:51 +0100

    drawable Object added and filled

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -1,3 +1,26 @@
-class DrawableObject extends World{
+class DrawableObject {
+  x = 120;
+  y = 330;
+  width = 120;
+  height = 50;
+  img;
+  imageCache = [];
+  currentImage = 0;
 
+  loadImg(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,10 +1,4 @@
-class MovableObject {
-  x = 120;
-  y = 330;
-  img;
-  width = 120;
-  height = 50;
-  imageCache = [];
+class MovableObject extends DrawableObject {
   currentImage = 0;
   speed = 0.15;
   otherDirection = false;
@@ -17,23 +11,6 @@ class MovableObject {
     bottom: 10,
     left: 10,
   };
-
-  loadImg(path) {
-    this.img = new Image();
-    this.img.src = path;
-  }
-
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageCache[path] = img;
-    });
-  }
-
-  draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
 
   drawFrame(ctx) {
     if (this instanceof Character || this instanceof Chicken) {
