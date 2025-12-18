ac99e49c6a1fdf23c2ebf21d55e8b5260c0a942d
KKrntzk <sophie.kornetzki@gmail.com>
2025-12-18 11:38:36 +0100

    coin animated

--- models/coins.class.js
+++ models/coins.class.js
@@ -12,17 +12,18 @@ class Coins extends DrawableObject {
 
   constructor() {
     super().loadImg("img_pollo_locco/img/8_coin/coin_1.png");
+    this.loadImages(this.images);
     this.x = 300 + Math.random() * 2000;
     this.y = 150 + Math.random() * 100;
-    // this.animateCoin();
+    this.animateCoin();
   }
 
-  //   animateCoin() {
-  //     setInterval(() => {
-  //       let i = this.currentImage % this.images.length;
-  //       let path = this.images[i];
-  //       this.img = this.imageCache[path];
-  //       this.currentImage++;
-  //     }, 300);
-  //   }
+  animateCoin() {
+    setInterval(() => {
+      let i = this.currentImage % this.images.length;
+      let path = this.images[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 300);
+  }
 }
