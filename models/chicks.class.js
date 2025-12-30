a98e30a41eaa7bd5f39342930709d9ba8a5419e9
KKrntzk <sophie.kornetzki@gmail.com>
2025-12-30 09:09:00 +0100

    dead animation added to small chicken

--- models/chicks.class.js
+++ models/chicks.class.js
@@ -3,6 +3,7 @@ class Chicks extends MovableObject {
   height = 30;
   width = 30;
   imagesWalking = ImageHub.chicks.walking;
+  imagesDead = ImageHub.chicks.dead;
 
   offset = {
     top: 5,
@@ -16,19 +17,29 @@ class Chicks extends MovableObject {
       "img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/1_w.png"
     );
     this.loadImages(this.imagesWalking);
+    this.loadImages(this.imagesDead);
     this.x = 200 + Math.random() * 500;
-    this.animate();
     this.speed = 0.35 + Math.random() * 0.9;
     this.dead = false;
+    this.isDying = false;
+    this.animate();
   }
 
   animate() {
     setInterval(() => {
-      this.moveLeft();
+      if (!this.dead && !this.isDying) {
+        this.moveLeft();
+      }
     }, 1000 / 60);
 
     setInterval(() => {
-      this.playAnimation(this.imagesWalking);
+      if (this.dead) return;
+
+      if (this.isDying) {
+        this.playAnimation(this.imagesDead);
+      } else {
+        this.playAnimation(this.imagesWalking);
+      }
     }, 200);
   }
 }
