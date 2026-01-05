f7e2fd7960db12ea869fb4bbd885c9298cb7cf04
KKrntzk <sophie.kornetzki@gmail.com>
2026-01-05 11:19:26 +0100

    audio hub optimized

--- models/audiohub.class.js
+++ models/audiohub.class.js
@@ -27,7 +27,6 @@ class AudioHub {
 
   static gameStart = new Audio("./sounds/game/gameStart.mp3");
 
-  // Array, das alle definierten Audio-Dateien enthält
   static allSounds = [
     AudioHub.characterDamage,
     AudioHub.characterDead,
@@ -45,34 +44,20 @@ class AudioHub {
     AudioHub.gameStart,
   ];
 
-  // Spielt eine einzelne Audiodatei ab
   static playOne(sound) {
-    sound.volume = 0.2; // Setzt die Lautstärke auf 0.2 = 20% / 1 = 100%
-    sound.currentTime = 0; // Startet ab einer bestimmten stelle (0=Anfang/ 5 = 5 sec.)
-    sound.play(); // Spielt das übergebene Sound-Objekt ab
+    sound.volume = 0.2;
+    sound.currentTime = 0;
+    sound.play();
   }
 
-  // Stoppt das Abspielen aller Audiodateien
   static stopAll() {
     AudioHub.allSounds.forEach((sound) => {
-      sound.pause(); // Pausiert jedes Audio in der Liste
+      sound.pause();
     });
-    // document.getElementById("volume").value = 0.2; // Setzt den Sound-Slider wieder auf 0.2
   }
 
-  // Stoppt das Abspielen einer einzelnen Audiodatei
   static stopOne(sound) {
-    sound.pause(); // Pausiert das übergebene Audio
-  }
-
-  // ##########################################################################################################################
-  // ################################################  Sound Slider - BONUS !  ################################################
-  // Setzt die Lautstärke für alle Audiodateien
-  static objSetVolume(volumeSlider) {
-    let volumeValue = document.getElementById("volume").value; // Holt den aktuellen Lautstärkewert aus dem Inputfeld
-    volumeSlider.forEach((sound) => {
-      sound.volume = volumeValue; // Setzt die Lautstärke für jedes Audio wie im Slider angegeben
-    });
+    sound.pause();
   }
 
   static mute() {
